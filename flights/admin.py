b8c72c51587b67c5b2d02d5a8e8e7a925dda831c
ahmed14-cell <ahmedito14@gmail.com>
2021-03-20 21:57:06 +0300

    refactor adminstratuve roles.

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,4 @@
+"""This script defines adminstrative tasks."""
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
@@ -7,24 +8,29 @@ from .models import User, Passenger, Flight, Staff
 
 
 class UserCreationForm(forms.ModelForm):
+    """class object to generate user creation forms."""
     class Meta:
+        """class"""
         model = User
         fields = ('username', 'email', 'user_type')
 
-    def save(self, commit=True):
-        # Save the provided password in hashed format
-        user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password"])
-        if commit:
-            user.save()
-        return user
+        def save(self, commit=True):
+            # Save the provided password in hashed format
+            user = super().UserCreationForm().save(commit=False)
+            user.set_password(self.cleaned_data["password"])
+            if commit:
+                user.save()
+            return user
 
 
 class CustomUserAdmin(UserAdmin):
+    """class to create user creation forms."""
     # The forms to add and change user instances
     add_form = UserCreationForm
-    list_display = ('username', 'email', 'user_type')  # fields to display in admin page while viewing all users
-    ordering = ("email",)  # order in which displayed
+    # fields to display in admin page while viewing all users
+    list_display = ('username', 'email', 'user_type')
+    # order in which displayed
+    ordering = ["email"]
 
     fieldsets = (
         (None, {'fields': ('email', 'password', 'first_name', 'last_name')}),
@@ -32,8 +38,10 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password', 'first_name', 'last_name', 'is_superuser', 'is_staff',
-                       'is_active', 'user_type')}
+            'fields': (
+                'username', 'email', 'password', 'first_name', 'last_name',
+                'is_superuser', 'is_staff', 'is_active', 'user_type'
+            )}
         ),
     )
 
